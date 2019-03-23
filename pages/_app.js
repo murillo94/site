40412dff20896cedb1b12ff580bd3441db526187
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-03-23 16:57:47 -0300

    Add GA

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+
+import Router from 'next/router';
+import withGA from 'next-ga';
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
+
+export default withGA('UA-136781679-1', Router)(MyApp);
