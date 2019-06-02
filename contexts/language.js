ad757078944b2d5cdd07b5817cbb94e523b88839
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-06-02 02:57:38 -0300

    Creating datas json about personal resume

--- contexts/language.js
+++ contexts/language.js
@@ -1,82 +1,35 @@
 import { createContext } from 'react';
 
+import personalData from '../data/personal';
+import educationData from '../data/education';
+import knowledgeData from '../data/knowledge';
+import jobsData from '../data/jobs';
+import languagesData from '../data/languages';
+
+import personalTranslations from '../translations/personal';
+import educationTranslations from '../translations/education';
+import knowledgeTranslations from '../translations/knowledge';
+import publicationsTranslations from '../translations/publications';
+import jobsTranslations from '../translations/jobs';
+import languageTranslations from '../translations/languages';
+
+const getTexts = language => ({
+  ...personalData[language],
+  ...personalTranslations[language],
+  ...educationData[language],
+  ...educationTranslations[language],
+  ...knowledgeData[language],
+  ...knowledgeTranslations[language],
+  ...publicationsTranslations[language],
+  ...jobsData[language],
+  ...jobsTranslations[language],
+  ...languagesData[language],
+  ...languageTranslations[language]
+});
+
 export const languages = {
-  en: {
-    title: "Murillo's Resume",
-    city: 'Joinville, SC, Brazil',
-    code: 'codes and contributions',
-    translation: 'translations',
-    first_intro:
-      'I have a experience ranging React, React Native, AngularJS to doing stuffs in backend with NodeJS, Python, Java.',
-    second_intro:
-      "I'm constanting learning/reading about things like folks, enviroments and tech.",
-    education: 'Education',
-    software: 'Software Engineering',
-    computer: 'Computer Engineering',
-    tec: 'Computer Technologist',
-    city_state: 'Joinville/Brazil',
-    love_work: 'Love working with',
-    love_content:
-      'Full Stack development, Performance First, UX principles, Cloud native technologies, CI/CD',
-    love_also: 'Also like',
-    love_also_content: 'Mobile development, Functional programming',
-    know_work: 'Knowledge',
-    lang: 'Languages',
-    learning: 'newbie learning for self study',
-    database: 'Databases',
-    platform: 'Platforms and Tools',
-    services: 'and others services',
-    others: 'Others',
-    publication: 'Publications',
-    exp: 'Experience',
-    current: 'Current',
-    exp1_position: 'Full Stack Developer',
-    exp1_desc:
-      'Helping companies to find out where target audience, explore new markets, control the team flow and sell more',
-    lng_speak: 'Language',
-    native: 'Native',
-    intermediate: 'Advanced (reading, speaking and writing)',
-    portuguese: 'Portuguese',
-    english: 'English'
-  },
-  ptbr: {
-    title: 'Resumo do Murillo',
-    city: 'Joinville, SC, Brasil',
-    code: 'códigos e contribuições',
-    translation: 'traduções',
-    first_intro:
-      'Tenho experiência desde React, React Native, AngularJS até desenvolvimento no backend com NodeJS, Python, Java.',
-    second_intro:
-      'Estou constantemente aprendendo/lendo sobre novas coisas como pessoas, ambientes de trabalho e tecnologia.',
-    education: 'Educação',
-    software: 'Engenharia de Software',
-    computer: 'Engenharia da Computação',
-    tec: 'Tecnólogo em Informática',
-    city_state: 'Joinville/Brasil',
-    love_work: 'Amo trabalhar com',
-    love_content:
-      'Desenvolvimento Full Stack, Performance First, Princípos UX, Tecnologias nativas da nuvem, CI/CD',
-    love_also: 'Também gosto',
-    love_also_content: 'Desenvolvimento mobile, Programação funcional',
-    know_work: 'Conhecimento',
-    lang: 'Linguagens',
-    learning: 'estudando',
-    database: 'Banco de dados',
-    platform: 'Plataformas e ferramentas',
-    services: 'e outros serviços',
-    others: 'Outros',
-    publication: 'Publicações',
-    exp: 'Experiência',
-    current: 'Atual',
-    exp1_position: 'Desenvolvedor Full Stack',
-    exp1_desc:
-      'Ajudando as empresas a encontrar novos clientes, explorar novos mercados, controlar o fluxo de atividades do time e vender mais',
-    lng_speak: 'Língua',
-    native: 'Nativo',
-    intermediate: 'Avançado (ler, falar e escrever)',
-    portuguese: 'Português',
-    english: 'Inglês'
-  }
+  en: getTexts('en'),
+  ptbr: getTexts('ptbr')
 };
 
 export const LanguageContext = createContext([languages.en, () => {}]);
--- pages/resume.js
+++ pages/resume.js
@@ -5,186 +5,178 @@ import Link from 'next/link';
 import Page from '../layouts/page';
 import Back from '../components/back';
 import Translate from '../components/translate';
+
+import publications from '../data/publications';
+
 import { LanguageContext, languages } from '../contexts/language';
 
 function Bio() {
   const context = useContext(LanguageContext)[0];
   const content = languages[context];
 
+  const cleanUrlPrint = url => url.replace('//', '');
+
   return (
     <article>
-      <h1>{content.title}</h1>
+      <h1>{content.personalTitle}</h1>
       <div>
-        <p>Murillo de Miranda Pereira</p>
+        <p>{content.name}</p>
         <p>{content.city}</p>
-        <p>murillomir0@gmail.com</p>
+        <p>{content.email}</p>
         <div className="link-bio">
-          <div className="show-print">
-            <Link href={'https://github.com/murillo94'}>
-              <a>Site</a>
-            </Link>
-            <span>- murillodmiranda.com</span>
-          </div>
-          <div>
-            <Link href={'https://www.linkedin.com/in/murillodmiranda/'}>
-              <a>Linkedin</a>
-            </Link>
-            <span className="show-print">
-              - linkedin.com/in/murillodmiranda
-            </span>
-          </div>
-          <div>
-            <Link href={'https://github.com/murillo94'}>
-              <a>Github</a>
-            </Link>
-            <span className="hide-print">- {content.code}</span>
-            <span className="show-print">- github.com/murillo94</span>
-          </div>
-          <div className="hide-print">
-            <Link
-              href={
-                'https://en.wikipedia.org/w/index.php?limit=50&title=Special%3AContributions&contribs=user&target=Murillodmiranda&namespace=&tagfilter=&start=&end='
-              }
-            >
-              <a>Wikipedia</a>
-            </Link>
-            <span>- {content.translation}</span>
-          </div>
-          <div className="hide-print">
-            <Link
-              href={
-                'https://developer.mozilla.org/pt-BR/profiles/murillodmiranda'
-              }
-            >
-              <a>MDN</a>
-            </Link>
-            <span>- {content.translation}</span>
-          </div>
-          <div className="hide-print">
-            <Link
-              href={
-                'https://stackoverflow.com/users/10746566/murillo-de-miranda-pereira'
-              }
-            >
-              <a>Stack Overflow</a>
-            </Link>
-          </div>
+          {content.links.map(({ name, href, description }) => {
+            if (name == 'Site') {
+              return (
+                <div className="show-print">
+                  <Link href={href}>
+                    <a>{name}</a>
+                  </Link>
+                  <span>- {href}</span>
+                </div>
+              );
+            }
+
+            if (name === 'Linkedin') {
+              return (
+                <div>
+                  <Link href={href}>
+                    <a>{name}</a>
+                  </Link>
+                  <span className="show-print">- {cleanUrlPrint(href)}</span>
+                </div>
+              );
+            }
+
+            if (name === 'Github') {
+              return (
+                <div>
+                  <Link href={href}>
+                    <a>{name}</a>
+                  </Link>
+                  <span className="hide-print">- {description}</span>
+                  <span className="show-print">- {cleanUrlPrint(href)}</span>
+                </div>
+              );
+            }
+
+            if (name === 'Stack Overflow') {
+              return (
+                <div className="hide-print">
+                  <Link href={href}>
+                    <a>{name}</a>
+                  </Link>
+                </div>
+              );
+            }
+
+            return (
+              <div className="hide-print">
+                <Link href={href}>
+                  <a>{name}</a>
+                </Link>
+                <span>- {description}</span>
+              </div>
+            );
+          })}
         </div>
       </div>
+
       <div>
-        <p>{content.first_intro}</p>
-        <p>{content.second_intro}</p>
+        {content.personalIntros.map(item => (
+          <p>{item}</p>
+        ))}
       </div>
+
       <div className="infos">
         <div>
-          <h2>{content.education}</h2>
-          <div>
-            <p>
-              Centro Universitário - Católica de Santa Catarina,{' '}
-              {content.city_state}
-            </p>
-            <ul>
-              <li>{content.software}</li>
-              <li>2017 / 2020</li>
-            </ul>
-          </div>
-          <div>
-            <p>
-              UniSociesc - Universidade Sociedade Educacional de Santa Catarina,{' '}
-              {content.city_state}
-            </p>
-            <ul>
-              <li>{content.computer}</li>
-              <li>2016 / 2016</li>
-            </ul>
-          </div>
-          <div>
-            <p>
-              UniSociesc - Educação e Tecnologia, Campus Boa Vista,{' '}
-              {content.city_state}
-            </p>
-            <ul>
-              <li>{content.tec}</li>
-              <li>2013 / 2015</li>
-            </ul>
-          </div>
+          <h2>{content.educationTitle}</h2>
+          {content.institutes.map(({ name, city, course, year }) => (
+            <div>
+              <p>
+                {name}, {city}
+              </p>
+              <ul>
+                <li>{course}</li>
+                <li>{year}</li>
+              </ul>
+            </div>
+          ))}
         </div>
+
         <div>
-          <h2>{content.love_work}</h2>
-          <p>{content.love_content}</p>
+          <h2>{content.loveWorkTitle}</h2>
+          <p>{content.loveContent}</p>
         </div>
+
         <div className="break-page" />
         <div className="margin-print">
-          <h2>{content.love_also}</h2>
-          <p>{content.love_also_content}</p>
+          <h2>{content.loveAlsoTitle}</h2>
+          <p>{content.loveAlsoContent}</p>
         </div>
+
         <div>
-          <h2>{content.know_work}</h2>
-          <p>
-            {content.lang}:
-            <span>
-              Javascript, Python, C++, Java, Elixir
-              <span>({content.learning})</span>
-            </span>
-          </p>
-          <p>
-            {content.database}:<span>DynamoDB, MySQL, MongoDB</span>
-          </p>
-          <p>
-            Frameworks / Libs:
-            <span>
-              ReactJS, Nextjs, React Native, ReasonReact, NodeJS, AngularJS,
-              Ionic
-            </span>
-          </p>
-          <p>
-            {content.platform}:
-            <span>
-              AWS (Lambda, S3, DynamoDB {content.services}), Git, Heroku, Fabric
-            </span>
-          </p>
-          <p>
-            {content.others}:
-            <span>CI/CD, TDD, SEO, APIs REST, RPC, XP, Scrum, Kanban</span>
-          </p>
+          <h2>{content.knowledgeTitle}</h2>
+          {content.knowledges.map(({ name, description }) => (
+            <p>
+              {name}:<span>{description}</span>
+            </p>
+          ))}
         </div>
+
         <div>
-          <h2>{content.publication}</h2>
+          <h2>{content.publicationsTitle}</h2>
           <ol>
-            <li>
-              <b>Murillo M. Pereira</b>, Gabriel Back, Nabor W. Júnior.
-              <i> "Arquitetura baseada em microserviço"</i>. The Open Software
-              Engineering Journal, 2018
-            </li>
+            {publications.map(({ me, others, title, publisher, year }) => (
+              <li>
+                <b>{me}</b>, {others}.<i> "{title}"</i>. {publisher}, {year}
+              </li>
+            ))}
           </ol>
         </div>
+
         <div>
-          <h2>{content.exp}</h2>
-          <p>
-            {content.exp1_position}
-            <span>
-              -
-              <Link href={'http://salespots.co/'}>
-                <a>SaleSpots</a>
-              </Link>
-              , {content.city_state}
-            </span>
-          </p>
-          <ul>
-            <li>{content.exp1_desc}</li>
-            <li>2016 / {content.current}</li>
-          </ul>
+          <h2>{content.jobsTitle}</h2>
+          {content.companies.map(
+            ({
+              position,
+              site,
+              name,
+              city,
+              descriptionCompany,
+              descriptionTech,
+              time
+            }) => (
+              <>
+                <p>
+                  {position}
+                  <span>
+                    -
+                    <Link href={site}>
+                      <a>{name}</a>
+                    </Link>
+                    , {city}
+                  </span>
+                </p>
+                <ul>
+                  <li>{descriptionCompany}</li>
+                  <li>{descriptionTech}</li>
+                  <li>{time}</li>
+                </ul>
+              </>
+            )
+          )}
         </div>
+
         <div>
-          <h2>{content.lng_speak}</h2>
-          <p>
-            {content.portuguese}
-            <span>- {content.native}</span>
-          </p>
-          <p>
-            {content.english}
-            <span>- {content.intermediate}</span>
-          </p>
+          <h2>{content.languageTitle}</h2>
+          {content.languages.map(({ name, level }) => (
+            <>
+              <p>
+                {name}
+                <span>- {level}</span>
+              </p>
+            </>
+          ))}
         </div>
       </div>
 
