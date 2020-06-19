120d1878cf11c3db9ae1b4bb163f8a04fb1d7c45
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-06-19 12:48:46 -0300

    Adding underline when link is selected

--- components/link.js
+++ components/link.js
@@ -1,43 +1,38 @@
 import { default as LinkNext } from "next/link";
 
-export const Link = ({ children, href, isExternal, isIcon }) => (
+export const Link = ({ children, href, isExternal, isIcon, isSelected }) => (
   <>
     {isExternal ? (
-      <a
-        className={isIcon && "icon"}
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href={href} target="_blank" rel="noopener noreferrer">
         {children}
       </a>
     ) : (
       <LinkNext href={href}>
-        <a className={isIcon && "icon"}>{children}</a>
+        <a>{children}</a>
       </LinkNext>
     )}
 
     <style jsx>{`
       a {
         font-size: 16px;
-        border-radius: 6px;
+        border-radius: ${isSelected ? "6px 6px 0 0" : "6px"};
+        box-shadow: ${isSelected ? "0 1px 0 0 currentColor" : "none"};
         padding: 5px 0;
-        transition: all 0.2s;
+        transition: background-color 0.2s;
+        display: ${isIcon ? "flex" : "initial"};
+        align-items: center;
       }
 
-      .icon {
-        display: flex;
-        align-items: center;
+      a:hover {
+        background-color: #efefef;
+        border-radius: 6px;
+        box-shadow: none;
       }
 
       @media (min-width: 768px) {
         a {
           font-size: 15px;
           padding: 6px 10px;
-        }
-
-        a:hover {
-          background: #efefef;
         }
       }
     `}</style>
