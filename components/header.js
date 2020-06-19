db04b3ce905715a5c3b989206c41afd0042e5da9
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-06-19 12:49:13 -0300

    Getting pathname to show link selected

--- components/header.js
+++ components/header.js
@@ -1,53 +1,65 @@
+import { useRouter } from "next/router";
+
 import { Link } from "./link";
 
 import Twitter from "../public/images/twitter.svg";
 import Circle from "../public/images/circle.svg";
 
-export const Header = () => (
-  <header>
-    <nav>
-      <div>
-        <Link href="/">Me</Link>
-        <Link href="/essays">Blog</Link>
-        <Link href="/resume">Resume</Link>
-      </div>
-      <div className="right-menu">
-        <Link href="https://twitter.com/murillodmiranda" isExternal isIcon>
-          <Twitter />
-        </Link>
-        <Link href="" isIcon>
-          <Circle />
-        </Link>
-      </div>
-    </nav>
+export const Header = () => {
+  const { pathname } = useRouter();
 
-    <style jsx>
-      {`
-        header {
-          padding: 48px 8px 0;
-          margin: 0 auto;
-          max-width: 768px;
-        }
+  return (
+    <header>
+      <nav>
+        <div>
+          <Link href="/" isSelected={pathname === "/"}>
+            Me
+          </Link>
+          <Link href="/essays" isSelected={pathname === "/essays"}>
+            Blog
+          </Link>
+          <Link href="/resume" isSelected={pathname === "/resume"}>
+            Resume
+          </Link>
+        </div>
+        <div className="right-menu">
+          <Link href="https://twitter.com/murillodmiranda" isExternal isIcon>
+            <Twitter />
+          </Link>
+          <Link href="" isIcon>
+            <Circle />
+          </Link>
+        </div>
+      </nav>
 
-        nav {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          flex-direction: row;
-        }
-
-        .right-menu {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-
-        @media print {
+      <style jsx>
+        {`
           header {
-            display: none;
+            padding: 48px 8px 0;
+            margin: 0 auto;
+            max-width: 768px;
           }
-        }
-      `}
-    </style>
-  </header>
-);
+
+          nav {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            flex-direction: row;
+          }
+
+          .right-menu {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+
+          @media print {
+            header {
+              display: none;
+            }
+          }
+        `}
+      </style>
+    </header>
+  );
+};
