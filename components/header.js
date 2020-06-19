dcc522192e257ff380adeed2fc24e48b8e6dff3d
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-06-19 11:37:04 -0300

    Adding in header twitter and dark mode svg

--- components/header.js
+++ components/header.js
@@ -1,11 +1,24 @@
 import { Link } from "./link";
 
+import Twitter from "../public/images/twitter.svg";
+import Circle from "../public/images/circle.svg";
+
 export const Header = () => (
   <header>
     <nav>
-      <Link href="/">Me</Link>
-      <Link href="/essays">Blog</Link>
-      <Link href="/resume">Resume</Link>
+      <div>
+        <Link href="/">Me</Link>
+        <Link href="/essays">Blog</Link>
+        <Link href="/resume">Resume</Link>
+      </div>
+      <div>
+        <Link href="https://twitter.com/murillodmiranda" isExternal>
+          <Twitter />
+        </Link>
+        <Link href="">
+          <Circle />
+        </Link>
+      </div>
     </nav>
 
     <style jsx>
@@ -14,6 +27,13 @@ export const Header = () => (
           padding: 48px 8px 0;
           max-width: 768px;
           margin: 0 auto;
+        }
+
+        nav {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          flex-direction: row;
         }
       `}
     </style>
