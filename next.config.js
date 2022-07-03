2667dd3fb2664acfb89fd19e46568557c9032d23
Murillo de Miranda <murillomir0@gmail.com>
2022-07-03 14:16:10 -0300

    feat: add plausible analytics

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const { withPlugins } = require('next-compose-plugins');
+const { withPlausibleProxy } = require('next-plausible');
 const withReactSvg = require('next-react-svg');
 const path = require('path');
 
-module.exports = withReactSvg({
+module.exports = withPlugins([withReactSvg, withPlausibleProxy], {
   include: path.resolve(__dirname, 'public/images'),
   webpack(config) {
     return config;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,14 +3,17 @@ import Head from 'next/head';
 import Router from 'next/router';
 import withGA from 'next-ga';
 import { ThemeProvider } from 'next-themes';
+import PlausibleProvider from 'next-plausible';
 
 const App = ({ Component, pageProps }) => (
-  <ThemeProvider>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
-    <Component {...pageProps} />
-  </ThemeProvider>
+  <PlausibleProvider domain="murillodmiranda.com">
+    <ThemeProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </PlausibleProvider>
 );
 
 export default withGA('UA-136781679-1', Router)(App);
